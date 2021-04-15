56282da5b9b211e73ca4905b6ae663120ae638e0
Andrew Wan <lwwnz@amazon.com>
2021-04-14 22:07:23 -0700

    Integrate Telemetry into language server

---
+++ PortingAssistantVSExtension/PortingAssistantExtensionServer/Models/PortingAssistantIDEConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using PortingAssistant.Client.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PortingAssistantExtensionServer.Models
+{
+    public class PortingAssistantIDEConfiguration
+    {
+        public PortingAssistantConfiguration portingAssistantConfiguration { get; set; }
+        public string metricsFilePath { get; set; }
+
+    }
+}

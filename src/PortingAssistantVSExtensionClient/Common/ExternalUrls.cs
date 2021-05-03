ce553bd7a8c5511853f88f91a56fe9d7c7cba844
Andrew Wan <flywanli@users.noreply.github.com>
2021-05-03 14:21:46 -0700

    Update ExternalUrls file
    Fix the email context

--- src/PortingAssistantVSExtensionClient/Common/ExternalUrls.cs
+++ src/PortingAssistantVSExtensionClient/Common/ExternalUrls.cs
@@ -10,7 +10,7 @@ namespace PortingAssistantVSExtensionClient.Common
     {
         public const string Agreement = "https://aws.amazon.com/agreement";
         public const string ServiceTerms = "https://aws.amazon.com/service-terms";
-        public const string SupportEmail = "aws-porting-assistant-support@amazon.com";
+        public const string SupportEmail = "mailto:aws-porting-assistant-support@amazon.com";
         public const string Documentation = "https://docs.aws.amazon.com/portingassistant/latest/userguide/what-is-porting-assistant.html";
         public const string HowItWorks = "https://docs.aws.amazon.com/portingassistant/latest/userguide/how-porting-assistant-works.html";
         public const string CollectInfomation= "https://docs.aws.amazon.com/portingassistant/latest/userguide/how-porting-assistant-works.html#porting-assistant-information-collected";

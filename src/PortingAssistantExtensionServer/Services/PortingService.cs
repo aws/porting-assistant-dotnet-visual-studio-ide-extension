63158e7919c65186e4ebe998b936a1ac5f9c663c
Huawen Mi <huawenm@amazon.com>
2021-08-10 17:46:40 -0700

    feat: added default upgrade package version for porting

--- src/PortingAssistantExtensionServer/Services/PortingService.cs
+++ src/PortingAssistantExtensionServer/Services/PortingService.cs
@@ -6,6 +6,7 @@ using PortingAssistantExtensionServer.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace PortingAssistantExtensionServer
 {
@@ -13,23 +14,60 @@ namespace PortingAssistantExtensionServer
     {
         private readonly ILogger<PortingService> _logger;
         private readonly IPortingAssistantClient _client;
+        public Dictionary<string, PackageAnalysisResult> PackageToAnalysisResults;
 
         public PortingService(ILogger<PortingService> logger,
     IPortingAssistantClient client)
         {
             _logger = logger;
             _client = client;
+            PackageToAnalysisResults = new Dictionary<string, PackageAnalysisResult>();
+        }
+
+        public async Task GetPackageAnalysisResultAsync(Task<SolutionAnalysisResult> SolutionAnalysisResultTask)
+        {
+            try
+            {
+                PackageToAnalysisResults = new Dictionary<string, PackageAnalysisResult>();
+                Task<SolutionAnalysisResult> solutionAnalysisResultTask = SolutionAnalysisResultTask;
+                var solutionAnalysisResult = await solutionAnalysisResultTask;
+
+                foreach (var projectAnalysisResult in solutionAnalysisResult.ProjectAnalysisResults)
+                {
+                    foreach (var packageAnalysisResultPair in projectAnalysisResult.PackageAnalysisResults)
+                    {
+                        var packageAnalysisResult = await packageAnalysisResultPair.Value;
+                        PackageToAnalysisResults[packageAnalysisResultPair.Key.PackageId] = packageAnalysisResult;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "get diagnostics failed with error: ");
+            }
         }
 
         public ProjectFilePortingResponse PortingProjects(ProjectFilePortingRequest request)
         {
             try
             {
+                var compatiblePacakges = PackageToAnalysisResults
+                    .Select(package => package.Value)
+                    .Where(packageAnalysisResult => packageAnalysisResult.CompatibilityResults.TryGetValue(request.TargetFramework, out var compatibilityResult) && compatibilityResult.Compatibility == Compatibility.COMPATIBLE)
+                    .ToList();
+
+                var packageToRecommendations = compatiblePacakges.Select(package => new PackageRecommendation()
+                {
+                    PackageId = package.PackageVersionPair.PackageId,
+                    Version = package.PackageVersionPair.Version,
+                    TargetVersions = new List<string> { package.CompatibilityResults[request.TargetFramework].CompatibleVersions.FirstOrDefault() },
+                    RecommendedActionType = RecommendedActionType.UpgradePackage,
+                });
                 var portingRequst = new PortingRequest
                 {
                     Projects = request.ProjectPaths.Select(p => new ProjectDetails() { ProjectFilePath = p }).ToList(),
                     SolutionPath = request.SolutionPath,
-                    RecommendedActions = new List<RecommendedAction>(),
+                    RecommendedActions = packageToRecommendations.Select(r => (RecommendedAction)r).ToList(),
                     TargetFramework = request.TargetFramework,
                     IncludeCodeFix = request.IncludeCodeFix
                 };

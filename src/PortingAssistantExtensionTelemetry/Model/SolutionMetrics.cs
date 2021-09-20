88072f83dddf9651f90d1199d32d22b25a55c876
Shanshan He <shanshanhebit@gmail.com>
2021-09-20 11:38:33 -0700

    feat: Add ApplicationGuid to IDE metrics.

--- src/PortingAssistantExtensionTelemetry/Model/SolutionMetrics.cs
+++ src/PortingAssistantExtensionTelemetry/Model/SolutionMetrics.cs
@@ -7,6 +7,10 @@ namespace PortingAssistantExtensionTelemetry.Model
     public class SolutionMetrics : MetricsBase
     {
         public string SolutionPath { get; set; }
+        public string ApplicationGuid { get; set; }
+        public string SolutionGuid { get; set; }
+        public string RepositoryUrl { get; set; }
+
         public double AnalysisTime { get; set; }
     }
 }

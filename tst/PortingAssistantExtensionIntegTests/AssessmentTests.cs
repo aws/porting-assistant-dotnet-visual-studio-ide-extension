499b1c2ce2556587be0df7a68433d488d9a7028e
Huawen Mi <huawenm@amazon.com>
2021-09-22 22:49:53 -0700

    fix: add test cases to fill integration test gaps

--- tst/PortingAssistantExtensionIntegTests/AssessmentTests.cs
+++ tst/PortingAssistantExtensionIntegTests/AssessmentTests.cs
@@ -12,6 +12,8 @@ namespace PortingAssistantExtensionIntegTests
 {
     class AssessmentTests
     {
+        public const string NETCOREAPP31 = "netcoreapp3.1";
+        public const string NET50 = "net5.0";
         private string tempProjectRoot;
         private List<string[]> testProjectInfoList = new List<string[]>();
         private string zipRootFolderPath;
@@ -26,6 +28,7 @@ namespace PortingAssistantExtensionIntegTests
             ["Miniblog.Core-master-PortResults.zip"] = new string[] { "Miniblog.Core-master-PortResults", "Miniblog.Core.sln", "Miniblog.Core.json" },
             ["MvcMusicStore-PortResults.zip"] = new string[] { "MvcMusicStore-PortResults", "MvcMusicStore.sln", "MvcMusicStore.json", "MvcMusicStorePort.json" },
             ["MvcMusicStore-WithFix-PortResults.zip"] = new string[] { "MvcMusicStore-WithFix-PortResults", "MvcMusicStore.sln", "MvcMusicStore.json", "MvcMusicStorePort.json" },
+            ["MvcMusicStore-net50-PortResults.zip"] = new string[] { "MvcMusicStore-net50-PortResults", "MvcMusicStore.sln", "MvcMusicStore.json", "MvcMusicStorePort.json" },
             ["NetFrameworkExample-PortResults.zip"] = new string[] { "NetFrameworkExample-PortResults", "NetFrameworkExample.sln", "NetFrameworkExample.json" }
         };
 
@@ -87,11 +90,11 @@ namespace PortingAssistantExtensionIntegTests
                 return;
             }
 
-            Boolean result = await TestSolutionAsync(projectInfo);
+            Boolean result = await TestSolutionAsync(projectInfo, NETCOREAPP31);
             Console.WriteLine("Verification TestMvcMusicStore Result: " + result);
             Assert.IsTrue(result);
 
-            Boolean portResult = await TestPortSolutionAsync(projectInfo, false);
+            Boolean portResult = await TestPortSolutionAsync(projectInfo, NETCOREAPP31, false);
             Console.WriteLine("Porting Verification TestMvcMusicStore Result: " + portResult);
             Assert.IsTrue(portResult);
         }
@@ -107,7 +110,7 @@ namespace PortingAssistantExtensionIntegTests
                 return;
             }
 
-            Boolean portResult = await TestPortSolutionAsync(projectInfo, false);
+            Boolean portResult = await TestPortSolutionAsync(projectInfo, NETCOREAPP31, false, false);
             Console.WriteLine("Porting Verification TestMvcMusicStore Result: " + portResult);
             Assert.IsFalse(portResult);
         }
@@ -123,27 +126,11 @@ namespace PortingAssistantExtensionIntegTests
                 return;
             }
 
-            Boolean result = await TestSolutionAsync(projectInfo);
+            Boolean result = await TestSolutionAsync(projectInfo, NETCOREAPP31);
             Console.WriteLine("Verification TestMvcMusicStore Result: " + result);
             Assert.IsTrue(result);
 
-            Boolean portResult = await TestPortSolutionAsync(projectInfo, true);
-            Console.WriteLine("Porting Verification TestMvcMusicStore Result: " + portResult);
-            Assert.IsTrue(portResult);
-        }
-
-        [Test]
-        public async Task TestMvcMusicStorePortingOnlyWithFixAsync()
-        {
-            InitializeTestResource("MvcMusicStore.zip");
-            string[] projectInfo = testProjectInfoList.FindLast(t => t[0].Equals("MvcMusicStore"));
-            if (projectInfo == null)
-            {
-                Assert.IsTrue(false);
-                return;
-            }
-
-            Boolean portResult = await TestPortSolutionAsync(projectInfo, true);
+            Boolean portResult = await TestPortSolutionAsync(projectInfo, NETCOREAPP31, true);
             Console.WriteLine("Porting Verification TestMvcMusicStore Result: " + portResult);
             Assert.IsTrue(portResult);
         }
@@ -158,11 +145,11 @@ namespace PortingAssistantExtensionIntegTests
                 return;
             }
 
-            Boolean result = await TestSolutionAsync(projectInfo);
+            Boolean result = await TestSolutionAsync(projectInfo, NETCOREAPP31);
             Console.WriteLine("Verification MiniblogCore Result: " + result);
             Assert.IsTrue(result);
 
-            Boolean portResult = await TestPortSolutionAsync(projectInfo, false);
+            Boolean portResult = await TestPortSolutionAsync(projectInfo, NETCOREAPP31, false);
             Console.WriteLine("Porting Verification TestMiniblogCore Result: " + portResult);
             Assert.IsTrue(portResult);
 
@@ -178,12 +165,32 @@ namespace PortingAssistantExtensionIntegTests
                 return;
             }
 
-            Boolean result = await TestSolutionAsync(projectInfo);
+            Boolean result = await TestSolutionAsync(projectInfo, NETCOREAPP31);
             Console.WriteLine("Verification NetFrameworkExample Result: " + result);
             Assert.IsTrue(result);
 
-            Boolean portResult = await TestPortSolutionAsync(projectInfo, false);
+            Boolean portResult = await TestPortSolutionAsync(projectInfo, NETCOREAPP31, false);
             Console.WriteLine("Porting Verification TestNetFrameworkExample Result: " + portResult);
+            Assert.IsTrue(portResult);
+        }
+
+        [Test]
+        public async Task TestMvcMusicStoreNet50Async()
+        {
+            InitializeTestResource("MvcMusicStore.zip");
+            string[] projectInfo = testProjectInfoList.FindLast(t => t[0].Equals("MvcMusicStore"));
+            if (projectInfo == null)
+            {
+                Assert.IsTrue(false);
+                return;
+            }
+
+            Boolean result = await TestSolutionAsync(projectInfo, NET50);
+            Console.WriteLine("Verification TestMvcMusicStore Result: " + result);
+            Assert.IsTrue(result);
+
+            Boolean portResult = await TestPortSolutionAsync(projectInfo, NET50, false);
+            Console.WriteLine("Porting Verification TestMvcMusicStore Result: " + portResult);
             Assert.IsTrue(portResult);
         }
 
@@ -196,7 +203,7 @@ namespace PortingAssistantExtensionIntegTests
             }
         }
 
-        private async Task<Boolean> TestSolutionAsync(string[] projectInfo)
+        private async Task<Boolean> TestSolutionAsync(string[] projectInfo, string targetFramework)
         {
             PAIntegTestClient client = null;
             try
@@ -210,7 +217,7 @@ namespace PortingAssistantExtensionIntegTests
                 client = new PAIntegTestClient(solutionPath, solutionName);
 
                 await client.InitClientAsync();
-                var currentResults = await client.AssessSolutionAsync();
+                var currentResults = await client.AssessSolutionAsync(targetFramework);
 
                 /* Uncomment this method to save the results in a json file*/
                 //SaveResults(jsonFile, currentResults);
@@ -246,22 +253,35 @@ namespace PortingAssistantExtensionIntegTests
             return false;
         }
 
-        private async Task<Boolean> TestPortSolutionAsync(string[] projectInfo, bool includeFix)
+        private async Task<Boolean> TestPortSolutionAsync(string[] projectInfo, string targetFramework, bool includeFix, bool assessSolution=true)
         {
             PAIntegTestClient client = null;
             try
             { 
                 string solutionPath = Path.Combine(tempProjectRoot, projectInfo[0]);
                 string solutionName = projectInfo[1];
+                string expectedSolutionPath = solutionPath;
 
                 StartLanguageServer();
 
                 client = new PAIntegTestClient(solutionPath, solutionName);
 
                 await client.InitClientAsync();
-                var currentResults = await client.PortSolutionAsync(includeFix);
+                if (assessSolution)
+                {
+                    await client.AssessSolutionAsync(targetFramework);
+                }
+                var currentResults = await client.PortSolutionAsync(targetFramework, includeFix);
+                if (includeFix)
+                {
+                    expectedSolutionPath += "-WithFix";
+                }
+                if(targetFramework.Equals(NET50))
+                {
+                    expectedSolutionPath += "-net50";
+                }
+                expectedSolutionPath += "-PortResults";
 
-                string expectedSolutionPath = includeFix ? solutionPath + "-WithFix-PortResults" : solutionPath + "-PortResults";
 
                 Boolean result = FileUtils.AreTwoDirectoriesEqual(solutionPath, expectedSolutionPath);
                 return result;

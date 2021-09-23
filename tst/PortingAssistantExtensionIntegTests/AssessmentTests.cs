0deaceeccac706434fbe14548ebca72730a13aa7
Huawen Mi <huawenm@amazon.com>
2021-09-22 23:11:59 -0700

    fix: cleanup after tests

--- tst/PortingAssistantExtensionIntegTests/AssessmentTests.cs
+++ tst/PortingAssistantExtensionIntegTests/AssessmentTests.cs
@@ -66,7 +66,7 @@ namespace PortingAssistantExtensionIntegTests
             }
         }
 
-        //[OneTimeTearDown]
+        [OneTimeTearDown]
         public void Cleanup()
         {
             ProcessHelper.getInstance().StopServer();

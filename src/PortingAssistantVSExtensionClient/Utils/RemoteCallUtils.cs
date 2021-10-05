8d28f949ce702b6a3a75c198c1f7806aac730790
Andrew Wan <lwwnz@amazon.com>
2021-10-05 15:36:12 -0700

    Remote process call utils

---
+++ src/PortingAssistantVSExtensionClient/Utils/RemoteCallUtils.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortingAssistantVSExtensionClient.Utils
+{
+    public static class RemoteCallUtils
+    {
+        // startinfo
+        /*         ProcessStartInfo(
+        RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false,
+            FileName = filename,
+            Arguments = args
+        ) */
+
+
+        private static ProcessStartInfo GetProcessInfo(String FileName, List<String> args, Boolean UseShellExecute = false, Boolean RedirectStandardOutput = true, Boolean RedirectStandardError = true)
+        {
+            return new ProcessStartInfo()
+            {
+                FileName = FileName,
+                Arguments = String.Join(" ", args),
+                RedirectStandardError = RedirectStandardError,
+                RedirectStandardOutput = RedirectStandardOutput,
+                UseShellExecute = UseShellExecute,
+            };
+        }
+    }
+}

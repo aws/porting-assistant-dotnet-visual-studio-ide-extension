752c6a976cfb804879aa6e792407d3d98237b4ab
Chris <longachr@amazon.com>
2021-10-15 16:26:19 -0700

    UI integ tests (#111)
    * test: initial checkin of ui tests
    
    * test: simplify test cases for pipeline
    
    * test: start winappdriver from test setup
    
    * test: fix one time setup
    
    move setup functions to assembly initialize method
    
    * test: click through vs first time setup
    
    * test: setup target framework and profile
    
    * test: set wait time before vs setup
    
    * test: aws profile and target framework selection and add param to apply file level changes during port
    
    * fix: first time aws profile setup for ui test
    
    * fix: package references in ui test project
    
    * test: add function to search filter error list
    
    * test: add wait time for VS splash screen
    
    * style: aws profile setup method and rename pa menu action
    
    * test: right click actions
    
    * test: adjust timeouts on session start
    
    * test: add wait time for vs app launch
    
    * test: longer wait time for assessment to complete
    
    * test: clean up ui testing

---
+++ tst/PortingAssistantExtensionUITests/Metric.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+
+namespace PortingAssistantExtensionUITests
+{
+    public class Metric
+    {
+        public string metricType;
+        public string metricName;
+        public string reference;
+        public string downloadedFile;
+        public string targetVersion;
+        public string solutionPath;
+        public string projectGuid;
+        public string packageName;
+        public string packageVersion;
+        public string actionName;
+        public string actionType;
+        public string actionValue;
+        public int timesRun;
+        public int invalidExecutions;
+        public string filePath;
+        public string buildErrorCode;
+        public string buildError;
+        public int count;
+    }
+}

2b7266a834237f0c4efde990286cb42b3394600c
Huawen Mi <huawenm@amazon.com>
2021-11-01 11:55:18 -0700

    fix view deployment

--- src/PortingAssistantVSExtensionClient/Dialogs/ViewDeploymentDialog.xaml.cs
+++ src/PortingAssistantVSExtensionClient/Dialogs/ViewDeploymentDialog.xaml.cs
@@ -28,15 +28,6 @@ namespace PortingAssistantVSExtensionClient.Dialogs
         {
             InitializeComponent();
             this.Title = "View Deployment";
-            DataTable dataTable = new DataTable();
-            DataColumn[] columns = { new DataColumn("ID"), new DataColumn("Value") };
-            Object[] row1 = { "1", "Value1" };
-            Object[] row2 = { "2", "Value2" };
-            Object[] row3 = { "3", "Value3" };
-            dataTable.Columns.AddRange(columns);
-            dataTable.Rows.Add(row1);
-            dataTable.Rows.Add(row2);
-            dataTable.Rows.Add(row3);
         }
 
         public static void Execute()

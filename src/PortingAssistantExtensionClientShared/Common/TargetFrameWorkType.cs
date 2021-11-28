ae05df6fdeaffb72f4d46314cd59b6e11aecf45a
huawenm <72728725+huawenm@users.noreply.github.com>
2021-11-27 19:29:15 -0800

    fix: add code comment

--- src/PortingAssistantExtensionClientShared/Common/TargetFrameWorkType.cs
+++ src/PortingAssistantExtensionClientShared/Common/TargetFrameWorkType.cs
@@ -12,6 +12,7 @@ namespace PortingAssistantVSExtensionClient.Common
 		public const string NETCOREAPP31 = "netcoreapp3.1";
 		public const string NET50 = "net5.0";
 		public const string NET60 = "net6.0";
+		// net6.0 only available on visual studio 2022
 		public static readonly List<string> ALL_SElECTION = new List<string>
 			 {
 				 NETCOREAPP31,
